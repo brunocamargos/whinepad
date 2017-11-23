740934e2091a0576a64d4bb81a44e824d3b69d3f
Bruno Camargos <bruno.camargos@belezanaweb.com.br>
2017-11-22 22:08:28 -0200

    remove unnecessary files created from build script

--- js/build/classification.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = {
-  grapes: ['Baco Noir', 'Barbera', 'Gabernet Franc', 'Gabernet Sauvignon']
-};
